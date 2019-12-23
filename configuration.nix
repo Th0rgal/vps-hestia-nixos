ea9d7ee1a31e5cad2cf311f212d3adf5046b56bf
Th0rgal <thomas.marchand@tuta.io>
2019-12-23 23:14:53 +0100

    :construction: test with python packages

--- configuration.nix
+++ configuration.nix
@@ -1,39 +1,49 @@
 { config, pkgs, ... }:
+with pkgs;
+let
+  my-python-packages = python-packages: with python-packages; [
+    aiohttp
+    pywal
+    pygame
+    pillow
+    requests
+  ];
+  python-with-my-packages = python3.withPackages my-python-packages;
+in
+  {
+    imports =
+      [
+        ./hardware-configuration.nix
+        ./networking.nix
+        ./users.nix
+      ];
 
-{
-  imports =
-    [
-      ./hardware-configuration.nix
-      ./networking.nix
-      ./users.nix
+    i18n = {
+      consoleFont = "Lat2-Terminus16";
+      consoleKeyMap = "fr";
+      defaultLocale = "fr_FR.UTF-8";
+    };
+
+    # Time zone
+    time.timeZone = "Europe/Zurich";
+
+    # Packages
+    environment.systemPackages = with pkgs; [
+      wget git python-with-my-packages
     ];
 
-  i18n = {
-     consoleFont = "Lat2-Terminus16";
-     consoleKeyMap = "fr";
-     defaultLocale = "fr_FR.UTF-8";
-  };
+    programs.fish.enable = true;
+    systemd.services = {
+        polymath = import ./services/polymath.nix { inherit pkgs; };
+    };
 
-  # Time zone
-  time.timeZone = "Europe/Zurich";
+    # System
+    boot = {
+      kernelParams = [ "net.ifnames=0" "biosdevname=0" ];
+      kernelPackages = pkgs.linuxPackages_latest;
+      cleanTmpDir = true;
+    };
+    system.stateVersion = "19.09";
 
-  # Packages
-  environment.systemPackages = with pkgs; [
-     wget git
-  ];
-
-  programs.fish.enable = true;
-  systemd.services = {
-      polymath = import ./services/polymath.nix { inherit pkgs; };
-  };
-
-  # System
-  boot = {
-    kernelParams = [ "net.ifnames=0" "biosdevname=0" ];
-    kernelPackages = pkgs.linuxPackages_latest;
-    cleanTmpDir = true;
-  };
-  system.stateVersion = "19.09";
-
-}
+  }
 
