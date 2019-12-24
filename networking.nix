0ea0e458569a3caf8f6150da55f859082b10cb07
Th0rgal <thomas.marchand@tuta.io>
2019-12-24 13:58:20 +0100

    :sparkles: adds IPV6 tunnel

--- networking.nix
+++ networking.nix
@@ -7,6 +7,13 @@
       allowPing = true;
       allowedTCPPorts = [ 8080 ];
     };
+
+    localCommands = ''
+    ip tunnel add he-ipv6 mode sit remote 216.66.84.42 ttl 255
+    ip link set he-ipv6 up
+    ip addr add 2001:470:1f12:328::2/64 dev he-ipv6
+    ip route add ::/0 dev he-ipv6 pref high
+    '';
   };
 
   # openssh daemon
